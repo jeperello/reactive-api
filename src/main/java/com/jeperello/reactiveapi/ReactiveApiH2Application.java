521224500b45f8eda2f7f1ef9dd099615d93a4a1
jeperello <jorgeperello2020@gmail.com>
2026-03-05 11:58:06 -0300

    Chore: initial project structure with WebFlux and R2DBC and H2

---
+++ src/main/java/com/jeperello/reactiveapi/ReactiveApiH2Application.java
@@ -0,0 +1,13 @@
+package com.jeperello.reactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeperello/reactiveapi/ReactiveApiH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeperello.reactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
