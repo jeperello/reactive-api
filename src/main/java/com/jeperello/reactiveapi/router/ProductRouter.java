9685814ebaeb347d4dea652d69beefd15a417421
jeperello <jorgeperello2020@gmail.com>
2026-03-07 10:21:03 -0300

    Adding redirect to tecnologies on home page

--- src/main/java/com/jeperello/reactiveapi/router/ProductRouter.java
+++ src/main/java/com/jeperello/reactiveapi/router/ProductRouter.java
@@ -8,6 +8,8 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import java.net.URI;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 @Configuration
@@ -16,9 +18,14 @@ public class ProductRouter {
     @Bean
     public RouterFunction<ServerResponse> route(ProductHandler handler, TechnologyHandler techHandler) {
         return RouterFunctions
-                .route(GET("/api/products"), handler::getAllProducts)
+                // Redirección de la raíz a las tecnologías
+                .route(GET("/"), request -> ServerResponse.temporaryRedirect(URI.create("/api/technologies")).build())
+
+                // Rutas de Productos
+                .andRoute(GET("/api/products"), handler::getAllProducts)
                 .andRoute(GET("/api/products/{id}"), handler::getProductById)
-                .andRoute(GET("/api/technologies"), techHandler::streamTechnologies)
-                ;
+
+                // Ruta de Tecnologías (Streaming)
+                .andRoute(GET("/api/technologies"), techHandler::streamTechnologies);
     }
 }
